24dde3711cf57d1ce84969ab76e8903b4c15ea46
Piotr Wendt <wendtpiotr333@gmail.com>
2022-03-28 21:02:46 +0200

    Final commit - Finishing App

--- src/components/HeaderInfo/HeaderInfo.js
+++ src/components/HeaderInfo/HeaderInfo.js
@@ -1,12 +1,12 @@
 import React from "react";
 import HeaderInfoStyles from "./HeaderInfoStyles.css";
 
-const HeaderInfo = () => {
+const HeaderInfo = (props) => {
   return (
     <div className="header-info">
       <h1>Expense Tracker</h1>
       <h3>Current Balance:</h3>
-      <h2>$$$</h2>
+      <h2>{props.balance.toFixed(2) + "$"}</h2>
     </div>
   );
 };
--- src/components/TransactionHistory/Transaction.js
+++ src/components/TransactionHistory/Transaction.js
@@ -1,7 +1,18 @@
 import React from "react";
+import TransactionStyles from "./TransactionStyles.css";
 
-const Transaction = () => {
-  return <div>Transaction 1</div>;
+const Transaction = (props) => {
+  const { message, value } = props;
+  return (
+    <div className="transaction-wrapper">
+      <div className="message">
+        <h3>"{message}"</h3>
+      </div>
+      <div className="amount">
+        <h3>{value}$</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Transaction;
